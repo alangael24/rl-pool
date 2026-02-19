2f31a7992578fb9fc12ba152651065a05d3569a8
alan <alangael2412@gmail.com>
2026-02-18 21:08:13 -0700

    pool: set max_physics_steps defaults to 96

--- pufferlib/ocean/pool/pool.c
+++ pufferlib/ocean/pool/pool.c
@@ -18,7 +18,7 @@ int main() {
         .reward_foul = -0.05f,
         .reward_win = 1.0f,
         .fast_forward = 0,
-        .max_physics_steps = 128,
+        .max_physics_steps = 96,
         .max_steps = 200,
     };
 
